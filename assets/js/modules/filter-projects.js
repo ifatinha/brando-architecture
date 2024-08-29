2be405f2a932cfa971d6c226eecf0c90b260f47f
ifatinha <ifatinha99@gmail.com>
2024-08-29 11:48:59 -0300

    funcão que filtra elementos da tela

--- assets/js/modules/filter-projects.js
+++ assets/js/modules/filter-projects.js
@@ -1,26 +1,30 @@
-function activeLinks() {
+export function filterProjects() {
+    const projects = document.querySelectorAll("[data-types]");
     const buttons = document.querySelectorAll("#project-links button");
+    const arrProjects = Array.from(projects);
 
-    const activeButtons = (index) => {
+    const activeButton = (index) => {
         buttons.forEach((button, i) => {
             button.classList.toggle("active-button-project", i === index);
         })
     }
 
+    const filterProjects = (button, index) => {
+        activeButton(index);
+        const datasetButton = button.dataset.type;
+
+        arrProjects.forEach(item => {
+            const isContains = item.dataset.types.includes(datasetButton);
+
+            if (!isContains) {
+                item.classList.toggle("remove-element");
+            }
+        })
+    }
+
     buttons.forEach((button, index) => {
-        button.addEventListener("click", () => activeButtons(index));
+        button.addEventListener("click", () => filterProjects(button, index));
     })
 
-    activeButtons(0);
-}
-
-export function filterProjects() {
-    const projects = document.querySelectorAll("[data-projects]");
-    const valores = projects[7].dataset.projects;
-    const arr = valores.length;
-
-    console.log(valores.substr(1, arr - 2));
-
-
-    activeLinks();
+    activeButton(0);
 }
